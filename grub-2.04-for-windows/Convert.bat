4524f68d3b3b49bd651b4d8c86cc536c0b32526b
Anish T <anish.t@ni.com>
2021-01-28 21:29:12 +0530

    added
    added

---
+++ grub-2.04-for-windows/Convert.bat
@@ -0,0 +1,4 @@
+﻿Rem Converts syslinux to grub
+grub-install.exe --force --no-floppy --target=i386-pc --boot-directory=E:\! //./PHYSICALDRIVE1
+grub-install.exe --force --removable --no-floppy --target=i386-efi --boot-directory=E:\! --efi-directory=E:\!
+grub-install.exe --force --removable --no-floppy --target=x86_64-efi --boot-directory=E:\! --efi-directory=E:\!
